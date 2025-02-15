c1474cc180e5328ba33705e18d57be38355959bf
prakhar <prakharmani24@gmail.com>
2025-02-15 15:52:26 +0530

    commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 fetch("/config")
   .then(response => response.json())
   .then(config => {
-      const JAVA_BACKEND_URL = config.backendUrl || "http://localhost:8080";
+      var JAVA_BACKEND_URL = config.backendUrl || "http://localhost:8080";
       console.log("Backend URL:", JAVA_BACKEND_URL);
   })
   .catch(error => console.error("Error fetching config:", error));
